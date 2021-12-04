428fab0012abaee27b4ee1248311a919d8de6e41
Burtorustum <burtorustum@gmail.com>
2021-12-04 13:26:04 -0500

    Day 2 Done

--- Problems/src/Day2.java
+++ Problems/src/Day2.java
@@ -8,12 +8,46 @@ public class Day2 extends AProblem {
 
   @Override
   String solvePartOne() {
-    return null;
+    int depth = 0;
+    int horizontal = 0;
+
+    for (String line : lines) {
+      String[] split = line.split(" ");
+      String command = split[0];
+      int num = Integer.parseInt(split[1]);
+
+      switch (command) {
+        case ("forward") -> horizontal += num;
+        case ("down") -> depth += num;
+        case ("up") -> depth -= num;
+      }
+    }
+
+    return (depth * horizontal) + "";
   }
 
   @Override
   String solvePartTwo() {
-    return null;
+    int depth = 0;
+    int horizontal = 0;
+    int aim = 0;
+
+    for (String line : lines) {
+      String[] split = line.split(" ");
+      String command = split[0];
+      int num = Integer.parseInt(split[1]);
+
+      switch (command) {
+        case ("forward") -> {
+          horizontal += num;
+          depth += aim * num;
+        }
+        case ("down") -> aim += num;
+        case ("up") -> aim -= num;
+      }
+    }
+
+    return (depth * horizontal) + "";
   }
 
   @Override
