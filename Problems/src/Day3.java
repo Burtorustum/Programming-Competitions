d4b75a0d9ab746267a03f3cc53b3a886d1f24e1c
Burtorustum <burtorustum@gmail.com>
2021-12-04 15:21:13 -0500

    Day 3 Done

---
+++ Problems/src/Day3.java
@@ -0,0 +1,105 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Day3 extends AProblem {
+
+  public Day3(String fileName) throws IOException {
+    super(fileName);
+  }
+
+  @Override
+  String solvePartOne() {
+    StringBuilder gamma = new StringBuilder();
+    StringBuilder epsilon = new StringBuilder();
+
+    for (int i = 0; i < lines.get(0).length(); i++) {
+      int zeroCount = 0;
+      int oneCount = 0;
+
+      for (String s : lines) {
+        if (s.charAt(i) == '0') {
+          zeroCount++;
+        } else {
+          oneCount++;
+        }
+      }
+
+      gamma.append(oneCount > zeroCount ? 1 : 0);
+      epsilon.append(zeroCount > oneCount ? 1 : 0);
+    }
+
+    return (Integer.parseInt(gamma.toString(), 2)
+        * Integer.parseInt(epsilon.toString(), 2))
+        + "";
+  }
+
+  @Override
+  String solvePartTwo() {
+    List<String> oxygenAccepted = new ArrayList<>(lines);
+    List<String> co2Accepted = new ArrayList<>(lines);
+    char oxygenMostCommon;
+    char co2LeastCommon;
+
+    int position = 0;
+    while (oxygenAccepted.size() != 1 || co2Accepted.size() != 1) {
+
+      int zeroCount = 0;
+      int oneCount = 0;
+
+      for (String s : oxygenAccepted) {
+        if (s.charAt(position) == '0') {
+          zeroCount++;
+        } else {
+          oneCount++;
+        }
+      }
+      oxygenMostCommon = zeroCount > oneCount ? '0' : '1';
+
+      zeroCount = 0;
+      oneCount = 0;
+
+      for (String s : co2Accepted) {
+        if (s.charAt(position) == '0') {
+          zeroCount++;
+        } else {
+          oneCount++;
+        }
+      }
+      co2LeastCommon = zeroCount > oneCount ? '1' : '0';
+
+      if (oxygenAccepted.size() > 1) {
+        oxygenAccepted = oxygenAccepted.stream()
+            .filter(new FilterPredicate(position, oxygenMostCommon))
+            .collect(Collectors.toList());
+      }
+
+      if (co2Accepted.size() > 1) {
+        co2Accepted = co2Accepted.stream()
+            .filter(new FilterPredicate(position, co2LeastCommon))
+            .collect(Collectors.toList());
+      }
+
+      position++;
+    }
+
+    return (Integer.parseInt(oxygenAccepted.get(0), 2)
+        * Integer.parseInt(co2Accepted.get(0), 2))
+        + "";
+  }
+
+  @Override
+  String name() {
+    return "--- Day 3: Binary Diagnostic ---";
+  }
+
+  private record FilterPredicate(int position, char mostCommon) implements Predicate<String> {
+    @Override
+    public boolean test(String s) {
+      return s.charAt(position) == mostCommon;
+    }
+  }
+
+}
--- Problems/src/Runner.java
+++ Problems/src/Runner.java
@@ -5,13 +5,18 @@ import java.util.List;
 public class Runner {
   public static void main(String[] args) throws IOException {
     List<AProblem> problems = new ArrayList<>();
-    problems.add(new Day1("data1.txt"));
-    problems.add(new Day2("data2.txt"));
+    problems.add(new Day1(nameFile(1)));
+    problems.add(new Day2(nameFile(2)));
+    problems.add(new Day3(nameFile(3)));
 
     for (AProblem p : problems) {
       System.out.println(p.name());
       System.out.println("Part 1: " + p.solvePartOne());
       System.out.println("Part 2: " + p.solvePartTwo());
     }
+  }
+
+  private static String nameFile(int day) {
+    return "data" + day + ".txt";
   }
 }
