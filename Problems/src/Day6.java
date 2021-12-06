6912fb89148b1a2f5d6cada6669f93f80fc784b6
Burtorustum <burtorustum@gmail.com>
2021-12-06 02:19:41 -0500

    Day 6 done

---
+++ Problems/src/Day6.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day6 extends AProblem {
+
+  public Day6(String fileName) throws IOException {
+    super(fileName);
+  }
+
+  @Override
+  String solvePartOne() {
+    return solve(80).toString();
+  }
+
+  @Override
+  String solvePartTwo() {
+    return solve(256).toString();
+  }
+
+  private BigInteger solve(int numDays) {
+    List<Integer> fish = Arrays.stream(lines.get(0).split(","))
+        .map(Integer::valueOf)
+        .collect(Collectors.toList());
+
+    BigInteger[] fishArr = new BigInteger[9];
+    for (int i = 0; i < 9; i++) {
+      fishArr[i] = new BigInteger("0");
+    }
+
+    for (int day : fish) {
+      fishArr[day] = fishArr[day].add(new BigInteger("1"));
+    }
+
+    for (int day = 1; day <= numDays; day++) {
+      iterate(fishArr);
+    }
+
+    BigInteger count = new BigInteger("0");
+    for (BigInteger i : fishArr) {
+      count = count.add(i);
+    }
+
+    return count;
+  }
+
+  private void iterate(BigInteger[] fishArr) {
+    BigInteger temp = fishArr[0];
+
+    fishArr[0] = fishArr[1];
+    fishArr[1] = fishArr[2];
+    fishArr[2] = fishArr[3];
+    fishArr[3] = fishArr[4];
+    fishArr[4] = fishArr[5];
+    fishArr[5] = fishArr[6];
+    fishArr[6] = fishArr[7].add(temp);
+    fishArr[7] = fishArr[8];
+    fishArr[8] = temp;
+  }
+
+  @Override
+  String name() {
+    return "--- Day 6: Lanternfish ---";
+  }
+}
