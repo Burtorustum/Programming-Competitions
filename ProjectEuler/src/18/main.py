e6b03168936e7652fa174e62e9e38a9c994067b5
Burtorustum <burtorustum@gmail.com>
2021-12-07 03:43:21 -0500

    Revert "Proj Euler structure cleanup"
    This reverts commit 58cadb5dd1a2a06e956ff3bd12d0bc0b349ec3d7.

--- ProjectEuler/src/18/main.py
+++ ProjectEuler/src/18/main.py
@@ -1,6 +1,6 @@
 def main():
-    #file = open("C:\\Users\\Burtorustum\\Desktop\\Project Euler\\Python.18\\triangle.txt", 'r')
-    file = open("C:\\Users\\Burtorustum\\Desktop\\Project Euler\\Python.18\\smalltriangle.txt", 'r')
+    #file = open("C:\\Users\\Burtorustum\\Desktop\\Project Euler\\18\\triangle.txt", 'r')
+    file = open("C:\\Users\\Burtorustum\\Desktop\\Project Euler\\18\\smalltriangle.txt", 'r')
     triangle = file.readlines()
     triangle = [x.split(" ") for x in triangle]
     triangle = [list(map(int, x)) for x in triangle]
