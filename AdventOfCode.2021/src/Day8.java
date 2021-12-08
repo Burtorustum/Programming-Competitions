2d1fa4978b96681bf881aa1c96eb59dcce91c15d
Burtorustum <burtorustum@gmail.com>
2021-12-08 01:44:29 -0500

    for some reason I didnt think to sort the strings  -_-

--- AdventOfCode.2021/src/Day8.java
+++ AdventOfCode.2021/src/Day8.java
@@ -1,9 +1,5 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day8 extends AProblem {
