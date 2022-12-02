9eda7256765128d3bc7da61c7bddd4067fa29ac2
aburur <burtorustum@gmail.com>
2022-12-02 00:38:03 -0500

    day 3 prep

--- AdventOfCode.2022/src/day2.py
+++ AdventOfCode.2022/src/day2.py
@@ -1,9 +1,9 @@
 from helper import getData
 input = getData(2).splitlines()
 
-rockScore = 1
-paperScore = 2
-scissorsScore = 3
+rock = 1
+paper = 2
+scissors = 3
 lose = 0
 win = 6
 tie = 3
@@ -13,32 +13,32 @@ sum2 = 0
 for line in input:
     chars = line.split(' ')
     if chars == ['A', 'X']:
-        sum1 += tie + rockScore
-        sum2 += scissorsScore + lose
+        sum1 += rock + tie
+        sum2 += scissors + lose
     elif chars == ['A', 'Y']:
-        sum1 += paperScore + win
-        sum2 += rockScore + tie
+        sum1 += paper + win
+        sum2 += rock + tie
     elif chars == ['A', 'Z']:
-        sum1 += scissorsScore + lose
-        sum2 += paperScore + win
+        sum1 += scissors + lose
+        sum2 += paper + win
     elif chars == ['B', 'X']:
-        sum1 += rockScore + lose
-        sum2 += rockScore + lose
+        sum1 += rock + lose
+        sum2 += rock + lose
     elif chars == ['B', 'Y']:
-        sum1 += paperScore + tie
-        sum2 += paperScore + tie
+        sum1 += paper + tie
+        sum2 += paper + tie
     elif chars == ['B', 'Z']:
-        sum1 += scissorsScore + win
-        sum2 += scissorsScore + win
+        sum1 += scissors + win
+        sum2 += scissors + win
     elif chars == ['C', 'X']:
-        sum1 += rockScore + win
-        sum2 += paperScore + lose
+        sum1 += rock + win
+        sum2 += paper + lose
     elif chars == ['C', 'Y']:
-        sum1 += paperScore + lose
-        sum2 += scissorsScore + tie
+        sum1 += paper + lose
+        sum2 += scissors + tie
     elif chars == ['C', 'Z']:
-        sum1 += scissorsScore + tie
-        sum2 += rockScore + win
+        sum1 += scissors + tie
+        sum2 += rock + win
 
 print("pt1:")
 print(sum1)
---
+++ AdventOfCode.2022/src/day3.py
@@ -0,0 +1,8 @@
+from helper import *
+input = getData(3).splitlines()
+test_input = getTestData(3).splitlines()
+
+print("pt1:")
+print()
+print("pt2:")
+print()
