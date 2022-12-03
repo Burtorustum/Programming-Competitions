4d564255e2c66ba38b518c1f88c691b9e706b214
Burtorustum <burtorustum@gmail.com>
2022-12-03 00:40:22 -0500

    day 3 done

--- AdventOfCode.2022/src/day3.py
+++ AdventOfCode.2022/src/day3.py
@@ -1,8 +1,32 @@
+import string
 from helper import *
 input = getData(3).splitlines()
 test_input = getTestData(3).splitlines()
 
+sum = 0
+for line in input:
+    one, two = line[:len(line)//2], line[len(line)//2:]
+    res = ""
+    for c in one:
+        if (two.count(c) > 0 and res.count(c) < 1):
+            res += c
+    for c in res:
+        sum += string.ascii_letters.index(c) + 1 
+
 print("pt1:")
-print()
+print(sum)
+
+sum = 0
+while len(input) != 0:
+    one, two, three = input[0], input[1], input[2]
+    del(input[:3])
+
+    res = ""
+    for c in one:
+        if (two.count(c) > 0 and three.count(c) > 0 and res.count(c) < 1):
+            res += c
+    for c in res:
+        sum += string.ascii_letters.index(c) + 1 
+
 print("pt2:")
-print()
+print(sum)
