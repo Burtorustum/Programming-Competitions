12324ca5e96a9a172208d3ac2a7cc86314f3cfd3
Burtorustum <burtorustum@gmail.com>
2022-12-06 00:31:28 -0500

    minor revision

--- AdventOfCode.2022/src/day6.py
+++ AdventOfCode.2022/src/day6.py
@@ -1,23 +1,21 @@
-import re
 from helper import *
 input = getData(6)
 
-target = 4
+target = 14
 p = target - 1
 
 prev = []
 for i in range(p):
     prev.append(input[i])
-    
-while True:
+
+count = 0
+while count != target:
     prev.append(input[p])
     p += 1
+    
     count = 0
     for c in prev:
-        if prev.count(c) == 1:
-            count += 1
-    if (count == target):
-        break
+        count += prev.count(c)
     prev.pop(0)
-    
+
 print(p)
